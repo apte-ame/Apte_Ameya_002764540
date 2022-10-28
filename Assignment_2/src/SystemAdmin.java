26029fc0cffea5d50266b158aecdda99b19f8881
apte-ame <apte.ame@northeastern.edu>
2022-10-28 12:08:15 -0400

    Adding updated SystemAdmin.java

---
+++ Assignment_2/src/SystemAdmin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author AMEYA A
+ */
+public class SystemAdmin {
+    private String userId;
+    private String password;
+    private String name; 
+}
+
+
