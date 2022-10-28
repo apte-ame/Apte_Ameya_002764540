282e17e24676414be980dae1c9e017b37c7e761c
apte-ame <apte.ame@northeastern.edu>
2022-10-28 12:11:25 -0400

    Updating structure for SystemAdmin

--- Assignment_2/src/model/SystemAdmin.java
+++ Assignment_2/src/model/SystemAdmin.java
@@ -2,15 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package model;
 
 /**
  *
  * @author AMEYA A
  */
 public class SystemAdmin {
-    private String userId;
-    private String password;
-    private String name; 
+    
 }
-
-
